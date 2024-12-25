aab7d90d4d1cb499a9db2cd05cda0cfbfe201358
Md Eusuf Uddin <eusuf.uddin@v2.ltd>
2024-12-26 01:48:57 +0600

    longest common prefix solution added

--- bin/longest_common_prefix.dart
+++ bin/longest_common_prefix.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 void main() {
-  Solution solution = Solution();
+  Solution3 solution = Solution3();
   print(solution.longestCommonPrefix(["flower", "flow", "flight"]));
 }
 
@@ -61,7 +63,7 @@ class Solution {
             if (j < second.length) {
               if (first[j] == second[j]) {
                 if (!output.contains(first[j])) {
-                  output = '${output}${first[j]}';
+                  output = '$output${first[j]}';
                 }
               }
             } else {
@@ -74,7 +76,7 @@ class Solution {
             if (m < first.length) {
               if (first[i] == second[i]) {
                 if (!output.contains(second[i])) {
-                  output = '${output}${first[i]}';
+                  output = '$output${first[i]}';
                 }
               }
             } else {
@@ -87,5 +89,62 @@ class Solution {
     }
 
     return output;
+  }
+}
+
+//["flower", "flow", "flight"]
+class Solution1 {
+  String longestCommonPrefix(List<String> strs) {
+    String prefixTmp = '';
+
+    for (int i = 0; i < strs.length; i++) {
+      String first = strs[i];
+
+      if (i == strs.length - 1) {
+        for (int j = 0; j < first.length && j < prefixTmp.length; j++) {
+          if (first[j] == prefixTmp[j]) {
+            prefixTmp += first[j];
+          } else {
+            prefixTmp = prefixTmp.replaceRange(j, prefixTmp.length, '');
+            break;
+          }
+        }
+        return prefixTmp;
+      }
+
+      String second = strs[i + 1];
+
+      for (int j = 0; j < first.length && j < second.length; j++) {
+        if (first[j] == second[j]) {
+          prefixTmp += first[j];
+        } else {
+          prefixTmp = prefixTmp.replaceRange(j, prefixTmp.length, '');
+          i++;
+          break;
+        }
+      }
+    }
+
+    return prefixTmp;
+  }
+}
+
+class Solution3 {
+  String longestCommonPrefix(List<String> strs) {
+    String prefixTmp = '';
+
+    strs.sort();
+
+    String f = strs[0];
+    String last = strs[strs.length - 1];
+
+    for (int i = 0; i < min(f.length, last.length); i++) {
+      if (f[i] != last[i]) {
+        break;
+      }
+      prefixTmp += f[i];
+    }
+
+    return prefixTmp;
   }
 }
